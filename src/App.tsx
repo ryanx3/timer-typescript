0b99f92e7725ae32c57746dd453459ffb2a246fc
Ryan <ryangabriel_cap@hotmail.com>
2024-04-09 07:49:57 +0100

    feat: Configurando estilos globais

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import { ThemeProvider } from "styled-components";
 import { Button } from "./components/Button";
 import { defaultTheme } from "./styles/themes/default";
+import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
-  <ThemeProvider theme={defaultTheme}>
-    <Button variant="primary"/>
-    <Button variant="secondary"/>
-    <Button variant="danger"/>
-    <Button variant="success"/>
-  </ThemeProvider>
-  )
+    <ThemeProvider theme={defaultTheme}>
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button variant="danger" />
+      <Button variant="success" />
+      <GlobalStyle />
+    </ThemeProvider>
+  );
 }
